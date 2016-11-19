002335af4b688b58c626c4f3b2e77e238b4d88a6
Joshua Bussdieker <jbussdieker@gmail.com>
2016-11-18 23:10:31 -0800

    Add providers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :providers
+
   root :to => 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161119071022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "providers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "type"
+    t.text     "configuration"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
