74e7166d990e71015250d2b44935ee684561043f
Joshua Bussdieker <jbussdieker@gmail.com>
2016-11-18 22:39:19 -0800

    Basic tmp dir for terraform

--- app/workers/terraform_worker.rb
+++ app/workers/terraform_worker.rb
@@ -2,6 +2,18 @@ class TerraformWorker
   include Sidekiq::Worker
 
   def perform(command)
-    puts `terraform #{command}`
+    with_tmpdir do
+      puts `terraform #{command} && pwd`
+    end
+  end
+
+  private
+
+  def with_tmpdir(&block)
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        yield
+      end
+    end
   end
 end
