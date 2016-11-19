8e1a9e46f948905d3d0199518a2d897b49a8de78
Joshua Bussdieker <jbussdieker@gmail.com>
2016-11-18 23:11:59 -0800

    Setup the provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,15 @@
 class Provider < ActiveRecord::Base
+  serialize :configuration
+
+  self.inheritance_column = nil
+
+  def configuration=(value)
+    if value.kind_of? String
+      super(YAML.load(value))
+    end
+  end
+
+  def to_h
+    { "provider" => { type => configuration } }
+  end
 end
